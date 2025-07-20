b51f1a54caeb7d9b659917e90b786d4b38de7a60
Jorge Peraza <jorge@monkeys.cloud>
2025-07-20 17:04:57 -0600

    fix: correct namespace

--- src/Middleware/JwtUserMiddleware.php
+++ src/Middleware/JwtUserMiddleware.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Middleware;
+namespace MonkeysLegion\Auth\Middleware;
 
 use MonkeysLegion\Auth\JwtService;
 use MonkeysLegion\Http\Message\JsonResponse;
